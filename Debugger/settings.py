63edee58eff93fd575e64d6310bac2e39d9ae90e
Qassem Alsaffar <qas.alsaffar@gmail.com>
2022-10-13 18:30:58 +0300

    First try

--- Debugger/settings.py
+++ Debugger/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'cinescapes',
+    'django.contrib.staticfiles',
+    'cinescape',
 ]
 
 MIDDLEWARE = [
--- cinescape/models.py
+++ cinescape/models.py
@@ -1,22 +1,21 @@
 from django.db import models
-from unittest.util import _MAX_LENGTH
 
-class Category(Model):
+class Category(models.Model):
     class CategoryType(models.TextChoices):
         GENERAL = "G"
         PARENTAL_GUIDANCE = "PG"
         ADULT_GUARDIAN = "R"
         RESTRICTED = "X"
 
-    rating = models.CharField(_MAX_LENGTH=1, choices=CategoryType)
-    name = models.CharField(_MAX_LENGTH=50)
+    rating = models.CharField(max_length=1, choices=CategoryType)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name + ' ' + self.rating.CategoryType
+        return f'''{self.name} + ' ' + {self.rating}.CategoryType'''
 
 
 class Movies(models.Model):
-    name = models.CharField(_MAX_LENGTH=50)
+    name = models.CharField(max_length=50)
     released_at = models.DateField()
     description = models.TextField()
     poster = models.URLField()
@@ -24,17 +23,17 @@ class Movies(models.Model):
     def __str__(self):
         return self.name 
 
-class Location(models):
-    geolocation = models.CharField(_MAX_LENGTH=250)
-    area_name = models.CharField(_MAX_LENGTH=50)
-    address_one = models.CharField(_MAX_LENGTH=250)
-    address_two = models.CharField(_MAX_LENGTH=250)
+class Location(models.Model):
+    geolocation = models.CharField(max_length=250)
+    area_name = models.CharField(max_length=50)
+    address_one = models.CharField(max_length=250)
+    address_two = models.CharField(max_length=250)
 
     def __str__(self):
         return self.name
 
-class Theater(Model.models):
-    name = models.CharField(_MAX_LENGTH=50)
+class Theater(models.Model):
+    name = models.CharField(max_length=50)
     capacity = models.PositiveIntegerField()
     booked_seats = models.PositiveIntegerField()
 
